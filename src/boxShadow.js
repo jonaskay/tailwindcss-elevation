2594126d0eb7c9ddb3e3e9585f7d65550bfd599a
jonaskay <jonaskay@iki.fi>
2019-03-18 21:06:31 +0200

    Calculate box-shadow values

---
+++ src/boxShadow.js
@@ -0,0 +1,18 @@
+const config = require('./config');
+
+module.exports = function boxShadow(z) {
+  if (typeof z !== 'number') {
+    return;
+  }
+  if (z < 0 || z > 24) {
+    return;
+  }
+
+  return (
+    [shadow('umbra'), shadow('penumbra'), shadow('ambient')].join(', ') + ';'
+  );
+
+  function shadow(type) {
+    return `${config.elevation[type][z]} rgba(0,0,0,${config.opacity[type]})`;
+  }
+};
---
+++ test/boxShadow.js
@@ -0,0 +1,42 @@
+const assert = require('chai').assert;
+
+const boxShadow = require('../src/boxShadow');
+
+describe('#boxShadow()', function() {
+  it('should return undefined when z is not present', function() {
+    assert.isUndefined(boxShadow());
+  });
+
+  it('should return undefined when z is not a number', function() {
+    assert.isUndefined(boxShadow('1'));
+  });
+
+  it('should return undefined when z is less than 0', function() {
+    assert.isUndefined(boxShadow(-1));
+  });
+
+  it('should return undefined when z is greater than 24', function() {
+    assert.isUndefined(boxShadow(25));
+  });
+
+  it('should return valid box-shadow value when z is 0', function() {
+    assert.equal(
+      boxShadow(0),
+      '0px 0px 0px 0px rgba(0,0,0,.2), 0px 0px 0px 0px rgba(0,0,0,.14), 0px 0px 0px 0px rgba(0,0,0,.12);'
+    );
+  });
+
+  it('should return valid box-shadow value when z is 1', function() {
+    assert.equal(
+      boxShadow(1),
+      '0px 2px 1px -1px rgba(0,0,0,.2), 0px 1px 1px 0px rgba(0,0,0,.14), 0px 1px 3px 0px rgba(0,0,0,.12);'
+    );
+  });
+
+  it('should return valid box-shadow value when z is 24', function() {
+    assert.equal(
+      boxShadow(24),
+      '0px 11px 15px -7px rgba(0,0,0,.2), 0px 24px 38px 3px rgba(0,0,0,.14), 0px 9px 46px 8px rgba(0,0,0,.12);'
+    );
+  });
+});
